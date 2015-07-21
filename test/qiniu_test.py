fb2d61d939746c99f6ab9b108f2009e1b1a0e1d8
cxy <cxymrzero@gmail.com>
2015-07-21 10:59:13 +0800

    remove test

--- test/qiniu_test.py
+++
@@ -1,7 +0,0 @@
-# Created by cxy on 15/4/14 with PyCharm
-# -*- coding: utf-8 -*-
-from qiniu import Auth
-
-access_key = '-Ho6-iKbkpJtMmhHi0q-DsvOgEH_ktLv2BUpDeXi'
-secret_key = 'fJF0skN1L6ZKholdidlrjkgHDV4B71ibmwdKXeJV'
-q = Auth(access_key, secret_key)
